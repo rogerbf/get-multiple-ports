64869c2aacbed6dce5ae3cf9d41f4d5f2fd4fce5
rogerbf <roger@bergfalk.nu>
2016-12-30 22:47:34 +0100

    initial

---
+++ source/library/getPort.test.js
@@ -0,0 +1,9 @@
+import getPort from './getPort'
+
+test(`getPort`, () => {
+  expect(typeof (getPort)).toEqual(`function`)
+  const server = {
+    address: jest.fn(() => ({ port: 3000 }))
+  }
+  expect(getPort(server)).toEqual(3000)
+})
