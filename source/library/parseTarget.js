3f0887f813517a417f676ec2428a33bdc687ec20
rogerbf <roger@bergfalk.nu>
2017-01-01 22:43:17 +0100

    rename to parseInput

--- source/library/parseTarget.js
+++
@@ -1,7 +0,0 @@
-export default target => {
-  const count = typeof (target) === `number` ? Array(target).fill({}) : null
-  const names = Array.isArray(target) ? target.map(name => ({ name })) : null
-  const name = typeof (target) === `string` ? [{ name: target }] : null
-  const empty = !target ? [{}] : null
-  return (count || names || name || empty)
-}
