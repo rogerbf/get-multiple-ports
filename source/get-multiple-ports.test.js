a99dfedb4fd7144bf74ff0dc658d416526373a53
rogerbf <roger@bergfalk.nu>
2017-04-10 23:15:16 +0200

    full coverage

--- source/get-multiple-ports.test.js
+++ source/get-multiple-ports.test.js
@@ -1,24 +1,63 @@
 const ports = require(`./get-multiple-ports`)
 
 describe(`ports`, () => {
-  it(`is a function`, () => {
-    expect(typeof (ports)).toEqual(`function`)
-  })
-
-  test(`happy path`, () => {
+  test(`ports(2)`, () => {
+    const testPorts = [ 9050, 9051 ]
     const createServer = jest.fn(() => ({
       unref: jest.fn(),
       on: jest.fn(),
       listen: jest.fn((port, fn) => fn()),
       address: jest.fn(() => {
-        return { port: 9050 }
+        return { port: testPorts.shift() }
       }),
       close: jest.fn(fn => fn())
     }))
 
-    return ports(createServer)
+    return ports(createServer, 2)
     .then(port => {
-      expect(port).resolves().toEqual(9050)
+      expect(port).toEqual([ 9050, 9051 ])
+    })
+    .catch(error => {
+      expect(error).toBeUndefined()
+    })
+  })
+
+  test(`ports([ 'one', 'two' ])`, () => {
+    const testPorts = [ 9050, 9051 ]
+    const createServer = jest.fn(() => ({
+      unref: jest.fn(),
+      on: jest.fn(),
+      listen: jest.fn((port, fn) => fn()),
+      address: jest.fn(() => {
+        return { port: testPorts.shift() }
+      }),
+      close: jest.fn(fn => fn())
+    }))
+
+    return ports(createServer, [ `one`, `two` ])
+    .then(port => {
+      expect(port).toEqual({ one: 9050, two: 9051 })
+    })
+    .catch(error => {
+      expect(error).toBeUndefined()
+    })
+  })
+
+  test(`ports([ 'one', undefined ])`, () => {
+    const testPorts = [ 9050, 9051 ]
+    const createServer = jest.fn(() => ({
+      unref: jest.fn(),
+      on: jest.fn(),
+      listen: jest.fn((port, fn) => fn()),
+      address: jest.fn(() => {
+        return { port: testPorts.shift() }
+      }),
+      close: jest.fn(fn => fn())
+    }))
+
+    return ports(createServer, [ `one`, undefined ])
+    .then(port => {
+      expect(port).toEqual([ { one: 9050 }, 9051 ])
     })
     .catch(error => {
       expect(error).toBeUndefined()
